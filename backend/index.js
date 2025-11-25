07c4515e2d0add41817cea71d55e31c8ec40ee33
Anjali-Chauhan1 <anjalichauhan1036@gmail.com>
2025-11-25 12:47:03 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,20 @@
 import app from "./app.js";
+import mongoose from "mongoose";
+import "dotenv/config"
+
+
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
+
 
 const PORT = process.env.PORT || 8080;
 
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,31 +1,21 @@
 import express from "express";
-import mongoose from "mongoose";
 import analysisRoutes from "./analysis.js";
 import chatRoutes from "./chat.js";
 import highlightRoutes from "./highlight.js";
 import riskRoutes from "./risk.js";
-import { signup,login } from "../controllers/authController.js";
-import "dotenv/config"
+import { signup, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.use("/signup", signup);
-router.use("/login", login);
+
+router.post("/signup", signup);
+router.post("/login", login);
+
+
 router.use("/analyze", analysisRoutes);
 router.use("/chat", chatRoutes);
 router.use("/highlight", highlightRoutes);
 router.use("/risk", riskRoutes);
-
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log("MongoDB connected");
-})
-.catch((err) => {
-    console.error("MongoDB connection error:", err);
-});
 
 export default router;
 
